1e0b13e4e7e163e6b3719a9aa742eaed1cdd1838
Peter McGowan <p.mcgowan1@gmail.com>
2019-02-23 00:24:01 +0000

    Update Problem 7

--- Problem7.py
+++ Problem7.py
@@ -4,29 +4,20 @@
 
 # Write a program that that takes a positive floating point number as input and outputs an approximation of its square root.
 
+import math # Import the math module, for sqrt function
+
 # user input, explicitly expects a positive floating point number, otherwise returns an error
-i = float(input('Please enter a positive integer: '))
+i = float(input('Please enter a positive number: '))
 
-# Check that integer is a positive integer
-if i > 0:
-    if i == 1:
-        print("That is a prime")
+# Check that float is positve
+if i < 0:
+    print("That is not a positive number") # If negative, print this message
 
-    else:
-
-        for j in range (2, i):
-            if i % j == 0:
-                print("That is not a prime")
-                break
-
-            else:
-                print("That is a prime")
-                break
-
-    
+elif i == 1:
+    print("The square root of 1 is 1") # If input number is 1 then print this
 
 else:
-    print("That is not a positive integer")
+    print("The square root of ", i, " is approx. ",round(math.sqrt(i),1),".",sep='') # In all other cases, print the message with the square root calculated by the math.sqrt function, rounded to 1 decimal place
 
 
 
