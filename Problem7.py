0fdcef63eea37d77eda33596a7ae78089747d14a
Peter McGowan <p.mcgowan1@gmail.com>
2019-03-13 22:34:19 +0000

    problem7.py updated to Newton method

--- Problem7.py
+++ Problem7.py
@@ -4,7 +4,7 @@
 
 # Write a program that that takes a positive floating point number as input and outputs an approximation of its square root.
 
-import math # Import the math module, for sqrt function
+j = 1.0 # Initial guess for calculation
 
 # user input, explicitly expects a positive floating point number, otherwise returns an error
 i = float(input('Please enter a positive number: '))
@@ -14,12 +14,11 @@ if i < 0:
     print("That is not a positive number") # If negative, print this message
 
 elif i == 1:
-    print("The square root of 1 is 1") # If input number is 1 then print this
+    print(f"The square root of 1 is 1") # Print only if 1 is input as the square
 
 else:
-    print("The square root of ", i, " is approx. ",round(math.sqrt(i),1),".",sep='') # In all other cases, print the message with the square root calculated by the math.sqrt function, rounded to 1 decimal place
+    while abs((j * j) - i) > 0.01: # WHile loop will iterate until difference between estimate squared and the input number is 0.01 or less
+        j -= ((j * j) - i) / (2 * j) # Implementation of Newton's method for approximating a square root
 
-
-
-
+    print(f"The square root of {i} is approx. {round(j,5)}") # In all other cases, print this approximation of the square root, rounded to 5 decimal places
 
