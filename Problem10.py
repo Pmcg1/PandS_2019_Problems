eafa1bbe918339730862a8d7066298925c85d70a
Peter McGowan <p.mcgowan1@gmail.com>
2019-03-24 14:55:23 +0000

    Completed Problem 8, more work done on Problem 10

--- Problem10.py
+++ Problem10.py
@@ -4,3 +4,48 @@
 
 # Write a program that displays a plot of the functions x, x2 and 2x in the range [0, 4].
 
+import matplotlib.pyplot as plt
+import numpy as np
+
+valueRange = list(range(0,4))
+print(valueRange)
+'''
+plt.figure(1, figsize=(18, 5))
+
+plt.subplot(131)
+plt.plot(valueRange, valueRange, color='red')
+plt.xlabel('x')
+plt.ylabel('x')
+plt.title("x vs x")
+plt.grid(True)
+
+plt.subplot(132)
+plt.plot(valueRange, np.square(valueRange), color='green')
+plt.xlabel('x')
+plt.ylabel('$x^2$')
+plt.title("x vs $x^2$")
+plt.grid(True)
+
+plt.subplot(133)
+plt.plot(valueRange, np.power(2,valueRange), color='blue')
+plt.xlabel('x')
+plt.ylabel('$2^x$')
+plt.title("x vs $2^x$")
+plt.grid(True)
+
+plt.suptitle('Problem 10')
+plt.show()
+'''
+
+
+fig, ax = plt.subplots()
+plt.plot(valueRange, valueRange, label='y = x', color='red')
+plt.plot(valueRange, np.square(valueRange), label='y = $x^2$', color='green')
+plt.plot(valueRange, np.power(2,valueRange), label='y = $2^x$', color='blue')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.grid(True)
+
+ax.legend()
+plt.suptitle('Problem 10')
+plt.show()
--- Problem8.py
+++ Problem8.py
@@ -6,7 +6,7 @@
 
 from datetime import datetime as dt # import datetime module
 
-i = dt.today() # current datetime is set as value of i
+i = dt.now() # current datetime is set as value of i
 
 def suffix(d): # if statement to set correct suffix for the date
     if d in (1, 21, 31):
@@ -18,5 +18,11 @@ def suffix(d): # if statement to set correct suffix for the date
     else:
         return "th"
 
-print(f'{i.strftime("%A, %B %d")}{suffix(i.day)} {i.year} at {i.strftime("%I:%M%p")}')
+currentDate = i.strftime("%d") # extract current date from current datetime, to allow for removal of any leading zero
+currentHour = i.strftime("%I") # extract current hour from current datetime, to allow for removal of any leading zero
+
+print(f'{i.strftime("%A, %B ")}{int(currentDate)}{suffix(i.day)} {i.year} at {int(currentHour)}{i.strftime(":%M%p")}')
+
+# f-string consisting of (full name of day), (full name of month) (date) (date suffix) (year) at (hour in 12hr clock):(minutes)(AM/PM)
+# both the current date and current hour are explicitly declared as integers in order to remove any leading zero they may have
 
