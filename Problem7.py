03b969a6a74ef5573a5a29441d056f6ebae6f533
Peter McGowan <p.mcgowan1@gmail.com>
2019-03-25 23:20:45 +0000

    Implementation of Newton's method rearranged and updated

--- Problem7.py
+++ Problem7.py
@@ -4,21 +4,20 @@
 
 # Write a program that that takes a positive floating point number as input and outputs an approximation of its square root.
 
-j = 1.0 # Initial guess for calculation
-
 # user input, explicitly expects a positive floating point number, otherwise returns an error
-i = float(input('Please enter a positive number: '))
+userInput = float(input('Please enter a positive number: '))
 
 # Check that float is positve
-if i < 0:
+if userInput < 0:
     print("That is not a positive number") # If negative, print this message
 
-elif i == 1:
+elif userInput == 1:
     print(f"The square root of 1 is 1") # Print only if 1 is input as the square
 
 else:
-    while abs((j * j) - i) > 0.01: # WHile loop will iterate until difference between estimate squared and the input number is 0.01 or less
-        j -= ((j * j) - i) / (2 * j) # Implementation of Newton's method for approximating a square root
+    currentApprox = 0.5 * userInput # Initial guess for calculation
 
-    print(f"The square root of {i} is approx. {round(j,5)}") # In all other cases, print this approximation of the square root, rounded to 5 decimal places
+    while abs((currentApprox**2)-userInput) > 0.01: # While loop will iterate until difference between estimate squared and the input number is 0.01 or less
+        currentApprox = 0.5 * (currentApprox+(userInput/currentApprox)) # Implementation of Newton's method for approximating a square root
 
+    print(f"The square root of {userInput} is approx. {round(currentApprox,3)}") # In all other cases, print this approximation of the square root, rounded to 3 decimal places
